2db8222271746382273c474b85406327b1e32ba8
Abdelrahman7896 <abdomedhat166@gmail.com>
2026-02-25 05:35:23 +0200

    end of part 6

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,29 +1,39 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { ToastService } from '../../core/services/toast-service';
+
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink,RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css',
 })
 export class Nav {
   protected accountServices =inject(AccountService)
- protected creds:any={}
+  private router = inject(Router);
+  private toast=inject(ToastService);
+  protected creds:any={}
+ 
  login(){
   this.accountServices.login(this.creds).subscribe({
-    next: result =>{
+    next: () =>{
       
-      console.log(result);
+      this.router.navigateByUrl('/members');
+      this.toast.success('logged in successfully');
       this.creds={};
     },
-    error:error=>alert(error.message)
+    error:error=>{
+      this.toast.error(error.error);
+    }
   })
  }
 
  logout(){
   this.accountServices.logout();
+  this.router.navigateByUrl('/');
 
  }
 }
